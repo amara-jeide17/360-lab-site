916ccfadc91818775c26afd16ec771e40f84b782
amara-jeide17 <46431533+amara-jeide17@users.noreply.github.com>
2025-04-29 15:27:46 -0700

    Add files via upload

--- db.php
+++ db.php
@@ -1,5 +1,9 @@
 <?php //basically following tutorial
-    $con = mysqli_connect("localhost","root","","pretend_photo_website"); 
+    $host = "byhmfdyzcaglrr9dwgdw-mysql.services.clever-cloud.com";
+    $user = "udagl77vtfqd1bua";
+    $password = "BOTzKJl0AuQriskKdELB";
+    $database = "byhmfdyzcaglrr9dwgdw";
+    $con = mysqli_connect($host,$user,$password,$database); 
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();//error handling
     }
